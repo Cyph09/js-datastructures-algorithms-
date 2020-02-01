778098140a856d7f74ccedca6d15cd9ac0d55b8f
Swaleh Mhina <cyph09@gmail.com>
2020-02-01 11:12:46 +0300

    Format the return results printout

--- search/searchSubStringNaive.js
+++ search/searchSubStringNaive.js
@@ -2,19 +2,19 @@ const substrSearchNaive = (str, subStr) => {
   let count = 0;
   for (let i = 0; i < str.length; i++) {
     for (let j = 0; j < subStr.length; j++) {
-      console.log(str[i + j], subStr[j]);
+      // console.log(str[i + j], subStr[j]);
       if (subStr[j] !== str[i + j]) {
-        console.log("BREAK!");
+        // console.log("BREAK!");
         break;
       }
       // End of inner loop is reached only when j == subStr.length -1
       if (j === subStr.length - 1) {
-        console.log("MATCH FOUND");
+        // console.log("MATCH FOUND");
         count++;
       }
     }
   }
-  return count;
+  return `Total matches of "${subStr}" found in "${str}" are: ${count}`;
 };
 
 module.exports = substrSearchNaive;
